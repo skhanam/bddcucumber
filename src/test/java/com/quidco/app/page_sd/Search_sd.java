8dc4639f4140d44e00ca3661590dd268890b8f62
Thadeus Ssewanyana <t.ssewanyana@quidco.com>
2016-04-26 17:28:13 +0100

    search feature

---
+++ src/test/java/com/quidco/app/page_sd/Search_sd.java
@@ -0,0 +1,19 @@
+package com.quidco.app.page_sd;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+/**
+ * Created by thadeus on 26/04/16.
+ */
+public class Search_sd {
+    @Given("^I enter a \"([^\"]*)\" on the rest url$")
+    public void i_enter_a_on_the_rest_url(String arg1) throws Throwable {
+
+    }
+
+    @Then("^I should get back a  merchant that \"([^\"]*)\" exactly matches the searchTerm$")
+    public void i_should_get_back_a_merchant_that_exactly_matches_the_searchTerm(String arg1) throws Throwable {
+
+    }
+}
