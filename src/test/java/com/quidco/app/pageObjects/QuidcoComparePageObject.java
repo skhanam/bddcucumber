420352f84307b16186a95d4b70af926452948185
Ashish Mohindroo <a.mohindroo@quidco.com>
2016-12-13 15:02:47 +0000

    removed unnecessary page object

--- src/test/java/com/quidco/app/pageObjects/QuidcoComparePageObject.java
+++ src/test/java/com/quidco/app/pageObjects/QuidcoComparePageObject.java
@@ -1,9 +1,7 @@
 package com.quidco.app.pageObjects;
 
-
 /**
- * Created by ashishmohindroo on 11/30/16.
+ * Created by ashishmohindroo on 12/15/16.
  */
 public class QuidcoComparePageObject {
-
 }
