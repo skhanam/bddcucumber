c75099ca05b1f3b0b38c306607465915bc99871e
skhanum <s.khanum@quidco.com>
2017-04-07 12:21:17 +0100

    Updated scroll as the button is hidden due to easter marketing promotion

--- src/test/java/com/quidco/app/pages/FavPage.java
+++ src/test/java/com/quidco/app/pages/FavPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by amohindroo on 02/03/17.
@@ -29,6 +30,9 @@ public class FavPage extends BasePage {
             e.click();
             waitForElementDisplay(favPageObject.removeBtn);
             scrollElementIntoMiddle(favPageObject.removeBtn);
+            sleep(TimeUnit.SECONDS,1);
+            scrollElementIntoMiddle(favPageObject.removeBtn);
+            sleep(TimeUnit.SECONDS,1);
             favPageObject.removeBtn.click();
             flwait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(FavPageObject.removebtnFormat)));
             numOfRetailers = favPageObject.expandBtns;
